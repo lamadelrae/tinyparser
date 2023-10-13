a482f7cde657ad83674a0821bafd1b681ecfd869
Lamadelrae <matthew.almeida572@gmail.com>
2023-10-13 01:03:43 -0300

    feature: adding api and action filter still need to fix documentation

---
+++ examples/TinyParser.Api.Example/Controllers/ExampleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace TinyParser.Api.Example.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExampleController : Controller
+{
+    [HttpGet]
+    [UseFilter<Person>]
+    public IActionResult Get([BindNever] Func<Person, bool> filter)
+    {
+        var people = new List<Person>()
+        {
+            new Person() { Name = "Matthew", Age = 22 },
+            new Person() { Name = "Lobato", Age = 21 },
+        };
+
+        return Ok(people.Where(filter));
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ examples/TinyParser.Api.Example/UseFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using TinyParser.Core.Generators.LINQ;
+
+namespace TinyParser.Api.Example;
+public class UseFilter<T> : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var filter = context.HttpContext.Request.Query["filter"];
+        if (filter.Count != 1) return;
+
+        var expression = LambdaFactory.Produce<T>(filter.First());
+
+        context.ActionArguments["filter"] = expression;
+
+        base.OnActionExecuting(context);
+    }
+}
